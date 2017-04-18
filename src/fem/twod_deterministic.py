59f8d81df0ee582725fa03e610977837ccbe39ba
Alex Carney <alcarneyorg@gmail.com>
2017-04-18 18:29:01 +0100

    Finished the better 2D deterministic code

--- src/fem/twod_deterministic.py
+++ src/fem/twod_deterministic.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import sqrt
+from scipy.sparse import csr_matrix
 
 
 def local_stiffness_matrix(a, b, c, h):
@@ -41,21 +43,112 @@ def construct_system(N, a, b, c):
     matrices A and M
     """
 
-    # Calculate h
+    # Calculate h, L, M
     h = 1/N
+    L = (N - 1)
+    P = (N + 1)
 
     # Get the local stiffness and mass matrices
-    A_k = local_stiffness_matrix(a, b, c, h)
-    M_k = local_mass_matrix(h)
+    Ak = local_stiffness_matrix(a, b, c, h)
+    Mk = local_mass_matrix(h)
 
-    # Consruct the global stiffness matrix
-    A = np.zeros((N - 1, N - 1))
-    i, j = np.indcies(A.shape)
+    # Consruct the global matrices
+    A = np.zeros((L**2, L**2))
+    M = np.zeros((L**2, P**2))
 
+    # Loop through each interior node, being careful of hitting boundary nodes
+    for i in range(L):
+        for j in range(L):
 
-    # Construct the global mass matrix
-    M = np.zeros(N - 1, N + 1)
-    i, j = np.indcies(M.shape)
+            # Compute k
+            k = i*L + j
+
+            M[k, (i + 1)*P + (j + 1)] = 2*(Mk[0, 0] + Mk[1, 1] + Mk[2, 2])
+            M[k, (i + 1)*P + (j + 2)] = Mk[0, 1] + Mk[1, 0]
+            M[k, (i + 1)*P + j] = Mk[1, 0] + Mk[0, 1]
+            M[k, i*P + (j + 1)] = Mk[0, 2] + Mk[2, 0]
+            M[k, (i+2)*P + (j + 1)] = Mk[2, 0] + Mk[0, 2]
+            M[k, i*P + (j + 2)] = Mk[1, 2] + Mk[2, 1]
+            M[k, (i+2)*P + j ] = Mk[2, 1] + Mk[1, 2]
+
+            A[k, k] = 2*(Ak[0, 0] + Ak[1, 1] + Ak[2, 2])
+
+            if (j + 1) <= (L - 1):
+                A[k, i*L + (j+1)] = Ak[0, 1] + Ak[1, 0]
+
+            if (j - 1) >= 0:
+                A[k, i*L + (j-1)] = Ak[1, 0] + Ak[0, 1]
+
+            if i - 1 >= 0:
+                A[k, (i-1)*L + j] = Ak[0, 2] + Ak[2, 0]
+
+            if i + 1 <= (L - 1):
+                A[k, (i+1)*L + j] = Ak[2, 0] + Ak[0, 2]
+
+            if j + 1 < (L - 1) and i - 1 >= 0:
+                A[k, (i-1)*L + (j+1)] = Ak[1, 2] + Ak[2, 1]
+
+            if j - 1 >= 0 and i + 1 <= (L - 1):
+                A[k, (i+1)*L + (j-1)] = Ak[2, 1] + Ak[1, 2]
+
+    # Convert the lil_matrix to csr_matrix for quicker solving
+#    A = csr_matrix(A)
+#    M = csr_matrix(M)
 
     return A, M
 
+
+def solve_system(f, N, a, b, c):
+    """
+    Given the density of the mesh N and function f representing the RHS
+    along with the coefficients a, b, c assemble the linear system Au = Mf and
+    solve for u
+    """
+
+    # Discretise the domain
+    xs = np.linspace(0, 1, N + 1)
+    ys = np.linspace(0, 1, N + 1)
+
+    # Approximate f
+    fs = np.array([f(x,y) for x in xs for y in ys])
+
+    # Construct the global matrices for the problem
+    print('Constructing...')
+    A, M = construct_system(N, a, b, c)
+
+    # Construct the RHS of Au = Mf
+    Mf = np.dot(M,fs)
+
+    # Solve the system
+    print('Solving...')
+    us = np.linalg.solve(A, Mf)
+
+    # Add in the boundary conditions
+    us = us.reshape((N - 1, N - 1))
+    U = np.zeros(tuple(n + 2 for n in us.shape))
+    U[tuple(slice(1, -1) for n in us.shape)] = us
+
+    return xs, ys, U
+
+
+def L2_error(u, U, N):
+    """
+    Given a function u representing the exact solution to the problem, an array
+    U representing our approximate solution to the problem and N representing
+    the density of the mesh, return the L2 norm of the error || u - U ||_2
+    """
+
+    # Initially assume zero error and recreate the domain
+    err = 0
+    xs, h = np.linspace(0, 1, N+1, retstep=True)
+    ys = np.linspace(0, 1, N+1)
+
+    # Add the error at each point
+    for i in range(len(xs)):
+        for j in range(len(ys)):
+            err += (u(xs[i], ys[j]) - U[i, j])**2
+
+    # Scale by the size of the triangles and square root
+    err = sqrt((h**2)*err)
+
+    return err
